f20d960e8739d5217da2deafaf7015154320e1ac
Dorian <dorianbuck@gmail.com>
2021-07-02 22:21:28 +0200

    1st succesfull

---
+++ my_group.rb
@@ -0,0 +1,4 @@
+my_group = ["person_1","person_2","person_3"]
+person_1 = {name:"Adam", gender:"male",age:"44"}
+person_2 = {name:"Bill", gender:"male",age:"42"}
+person_3 = {name:"Cilly", gender:"female",age:"423"}
