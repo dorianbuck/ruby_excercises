95ccbd61398f367139c92f41dcaea5d648570c7b
Dorian <dorianbuck@gmail.com>
2021-07-02 23:29:23 +0200

    beg challange

--- my_group.rb
+++ my_group.rb
@@ -2,3 +2,7 @@ my_group = ["person_1","person_2","person_3"]
 person_1 = {name:"Adam", gender:"male",age:"44"}
 person_2 = {name:"Bill", gender:"male",age:"42"}
 person_3 = {name:"Cilly", gender:"female",age:"423"}
+names = ['Thomas', 'Noel', 'Faraz']
+names.each do |name|
+puts "Hello #{name}"
+end
