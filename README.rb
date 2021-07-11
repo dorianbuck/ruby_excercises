bfb6b6e5088ac3ab871fdead0f01052559607a08
Dorian <dorianbuck@gmail.com>
2021-07-11 22:56:55 +0200

    ruby 4th

---
+++ README.rb
@@ -0,0 +1,3 @@
+#SyntaxError: (irb):2: syntax error, unexpected ')', expected '}'
+
+#This type of error msg is most likely caused by using the wrong bracket in your code..
---
+++ movie_db.rb
@@ -0,0 +1,10 @@
+movies = {
+    Hajen: 1975, 
+    Snövit: 1989,
+    Matrix: 1998,
+    Casablanca: 1963,
+    Braindead: 1991
+}
+movies.each do |key, value|
+puts value
+end
