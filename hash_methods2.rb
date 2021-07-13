d429c80b176ebed20ea79953320a74cc2495d65e
Dorian Buck <[dorianbuck@gmail.com]>
2021-07-13 19:40:00 +0200

    ruby11th

---
+++ hash_methods2.rb
@@ -0,0 +1,5 @@
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+print my_hash, my_hash2
+#my_hash stores key instead of string
---
+++ method1.rb
@@ -0,0 +1,10 @@
+def  caps(string)  
+    if string.length >=  10
+      string.upcase 
+    else 
+      string 
+    end  
+  end 
+  
+  puts  caps("Hi world")
+  puts  caps("Hello world")
